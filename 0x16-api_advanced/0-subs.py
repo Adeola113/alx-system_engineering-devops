c2a08e1342a0c2c8b5fd258c1248211910a243e6
Adeola113 <adeolabello537@gmail.com>
2023-08-08 16:39:57 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers fir a given subreddit"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
     r  requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
